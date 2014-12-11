f7772e1a77c5627fa99f9752e066b4083aaaf7a0
Michał <michal_kopaczewski@o2.pl>
2014-12-11 20:17:34 +0100

    Utworzenie gniazda oraz oczekiwanie na klienta

--- src/serwer/Serwer.java
+++ src/serwer/Serwer.java
@@ -8,6 +8,7 @@ public class Serwer {
     public static void main(String[] args) {
         ServerSocket gniazdoSerwer = null;
         Socket polaczenie = null;
+        OutputStream komunikatDlaKlienta = null;
         try {
             gniazdoSerwer = new ServerSocket(8866);
             
@@ -18,9 +19,31 @@ public class Serwer {
         }
         try {
             polaczenie = gniazdoSerwer.accept();
+            System.out.printf("oczekiwanie polaczenia z klientem");
         } catch (IOException ex) {
             System.out.printf("Nie mozna nawiazac polaczenia z klientem");
-            Logger.getLogger(Serwer.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(Serwer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            komunikatDlaKlienta = polaczenie.getOutputStream();
+            PrintWriter zapis = null;
+            if (komunikatDlaKlienta != null) {
+                zapis = new PrintWriter(komunikatDlaKlienta);
+                zapis.print("Czesc kliencie, to ja serwer");
+                zapis.close();
+                
+            }
+            System.out.println("Pobranie strumienia wyjscia do klienta");
+        } catch (IOException ex) {
+            System.out.println("Blad w pobieraniu strumienia wyjscia");
+            //Logger.getLogger(Serwer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            polaczenie.close();
+            System.out.println("Zamknięcie połączenia"); 
+        } catch (IOException ex) {
+            System.out.println("Nie można zamknąć połączenia."); 
+            //Logger.getLogger(Serwer.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
